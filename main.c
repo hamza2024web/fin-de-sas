5fb0ff71cc24f1288f2155d712f38670af9d02e7
hamza2024web <hamzaakroubi1528@gmail.com>
2024-10-03 00:39:43 +0100

    Update main.c
    bla fonction

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+struct date_de_reservation {
+    char jour[3]; 
+    char mois[3]; 
+    char annee[5];
+};
+
 struct dentaire {
     char nom[50];
     char prenom[50];
@@ -8,157 +15,133 @@ struct dentaire {
     char age[50];
     int statut;
     char reference[50];
-    struct date_de_reservation{
-        char jour;
-        char mois;
-        char annee;
-    };
+    struct date_de_reservation date;
 };
-void initiale_reservation(int p1[100]){
-    for(int i=0;i<9;i++){
-        strcpy(p1.nom,"el ourari");
-        strcpy(p1.prenom,"ilyass");
-        strcpy(p1.telephone,"0611223344");
-        p1.age=18;
-        strcpy(p1.status,"validé");
-        strcpy(p1.reference,"1");
-        strcpy(p1.jour,"01");
-        strcpy(p1.mois,"01");
-        strcpy(p1.annee,"2024");
-        strcpy(p1.nom,"madada");
-        strcpy(p1.prenom,"mohamad");
-        strcpy(p1.telephone,"0611484515");
-        p1.age=18;
-        strcpy(p1.status,"validé");
-        strcpy(p1.reference,"2");
-        strcpy(p1.jour,"02");
-        strcpy(p1.mois,"01");
-        strcpy(p1.annee,"2024");
-        strcpy(p1.nom,"laissi");
-        strcpy(p1.prenom,"abdellah");
-        strcpy(p1.telephone,"061857423344");
-        p1.age=21;
-        strcpy(p1.status,"validé");
-        strcpy(p1.reference,"3");
-        strcpy(p1.jour,"03");
-        strcpy(p1.mois,"01");
-        strcpy(p1.annee,"2024");
-        strcpy(p1.nom,"oubab");
-        strcpy(p1.prenom,"khalid");
-        strcpy(p1.telephone,"06185424444");
-        p1.age=18;
-        strcpy(p1.status,"traite");
-        strcpy(p1.reference,"4");
-        strcpy(p1.jour,"04");
-        strcpy(p1.mois,"01");
-        strcpy(p1.annee,"2024");
-        strcpy(p1.nom,"pikal");
-        strcpy(p1.prenom,"arinas");
-        strcpy(p1.telephone,"0611032569");
-        p1.age=25;
-        strcpy(p1.status,"reporté");
-        strcpy(p1.reference,"5");
-        strcpy(p1.jour,"05");
-        strcpy(p1.mois,"01");
-        strcpy(p1.annee,"2024");
-        strcpy(p1.nom,"talibi");
-        strcpy(p1.prenom,"aimad");
-        strcpy(p1.telephone,"061155889944");
-        p1.age=18;
-        strcpy(p1.status,"validé");
-        strcpy(p1.reference,"6");
-        strcpy(p1.jour,"06");
-        strcpy(p1.mois,"01");
-        strcpy(p1.annee,"2024");
-        strcpy(p1.nom,"alidai");
-        strcpy(p1.prenom,"ali");
-        strcpy(p1.telephone,"0785463219");
-        p1.age=18;
-        strcpy(p1.status,"validé");
-        strcpy(p1.reference,"6");
-        strcpy(p1.jour,"06");
-        strcpy(p1.mois,"01");
-        strcpy(p1.annee,"2024");
-        strcpy(p1.nom,"el ourrari");
-        strcpy(p1.prenom,"lina");
-        strcpy(p1.telephone,"0511223344");
-        p1.age=30;
-        strcpy(p1.status,"validé");
-        strcpy(p1.reference,"7");
-        strcpy(p1.jour,"07");
-        strcpy(p1.mois,"01");
-        strcpy(p1.annee,"2024");
-        strcpy(p1.nom,"el malki");
-        strcpy(p1.prenom,"abderrahmane");
-        strcpy(p1.telephone,"061122070809");
-        p1.age=27;
-        strcpy(p1.status,"annule");
-        strcpy(p1.reference,"8");
-        strcpy(p1.jour,"08");
-        strcpy(p1.mois,"01");
-        strcpy(p1.annee,"2024");
-        strcpy(p1.nom,"el ourari");
-        strcpy(p1.prenom,"khadija");
-        strcpy(p1.telephone,"0611778844");
-        p1.age=42;
-        strcpy(p1.status,"validé");
-        strcpy(p1.reference,"9");
-        strcpy(p1.jour,"09");
-        strcpy(p1.mois,"01");
-        strcpy(p1.annee,"2024");
-        strcpy(p1.nom,"ouasserkho");
-        strcpy(p1.prenom,"abdelhafid");
-        strcpy(p1.telephone,"05287963147");
-        p1.age=19;
-        strcpy(p1.status,"validé");
-        strcpy(p1.reference,"10");
-        strcpy(p1.jour,"10");
-        strcpy(p1.mois,"01");
-        strcpy(p1.annee,"2024");
-    }
-}
-int main()
-{
+int main() {
     struct dentaire p1[100];
     int choice1;
     int choice2;
     int numbre_reser;
-    do{
-        printf("\tmenu de reservation");
-        printf("1/.Ajout de reservation .\n");
-        printf("2/.modifier une reservation .\n");
-        printf("3/.supprimer une reservation .\n");
-        printf("4/.affiche les details d'une reservation .\n");
-        printf("5/.Tri des reservation .\n");
-        printf("6/.Recherche des reservation .\n");
-        printf("7/.les statistiques .\n");
-        printf("veuillez saiser ton choice .\n");
-        scanf("%d",choice1);
-        switch(choice){
-        case 1 :
-            printf("ton choice est d'ajouter une reservation .\n");
-            printf("combien du reservation souhaitez-vous ajoutez ? :");
-            scanf("%d",&numbre_reser);
-            for(int i=9;i<numbre_reser;i++){
-                printf("veuillez saiser le nom de patient :");
-                gets(p1[i].nom);
-                printf("veuillez saiser le prenom de patient :");
-                gets(p1[i].prenom);
-                printf("veuillez saiser le numero de telephone de patient :");
-                gets(p1[i].telephone);
-                printf("veuillez saiser l'age de patient :");
-                gets(p1[i].age);
-                printf("veuillez saiser le status de patient :");
-                printf("\tMenu de status\n");
-                printf("1/.validé .\n");
-                printf("2/.reporté .\n");
-                printf("3/.annulé .\n");
-                printf("4/.traité .\n");
-                printf("veuillez saiser ton choice .");
-                scanf("%d",&p1.status[i]);
-
-        }
-    }
-    }
+    char mod_par_refer[50];
+    do {
+        printf("\tMenu de reservation\n");
+        printf("1/. Ajouter une reservation.\n");
+        printf("2/. Modifier une reservation.\n");
+        printf("3/. Supprimer une reservation.\n");
+        printf("4/. Afficher les details d'une reservation.\n");
+        printf("5/. Tri des reservations.\n");
+        printf("6/. Recherche des reservations.\n");
+        printf("7/. Les statistiques.\n");
+        printf("Veuillez saisir ton choix: ");
+        scanf("%d", &choice1);
+        getchar(); 
+        switch (choice1) {
+            case 1:
+                printf("Ton choix est d'ajouter une reservation.\n");
+                printf("Combien de reservations souhaitez-vous ajouter ? : ");
+                scanf("%d", &numbre_reser);
+                getchar(); 
+                for (int i = 0; i < numbre_reser; i++) {
+                    printf("Veuillez saisir le nom de patient : ");
+                    fgets(p1[i].nom, sizeof(p1[i].nom), stdin);
+                    printf("Veuillez saisir le prenom de patient : ");
+                    fgets(p1[i].prenom, sizeof(p1[i].prenom), stdin);
+                    printf("Veuillez saisir le numero de telephone de patient : ");
+                    fgets(p1[i].telephone, sizeof(p1[i].telephone), stdin);
+                    printf("Veuillez saisir l'age de patient : ");
+                    fgets(p1[i].age, sizeof(p1[i].age), stdin);
+                    printf("Veuillez saisir le status de patient :\n");
+                    printf("1/. ValidÃ©.\n");
+                    printf("2/. ReportÃ©.\n");
+                    printf("3/. AnnulÃ©.\n");
+                    printf("4/. TraitÃ©.\n");
+                    printf("Veuillez saisir ton choix : ");
+                    scanf("%d", &p1[i].statut);
+                    getchar(); 
+                    printf("Veuillez saisir le reference de patient :");
+                    fgets(p1[i].reference, sizeof(p1[i].reference), stdin);
+                    printf("Veuillez saisir la date de reservation (jour mois annee) : \n");
+                    printf("Jour : ");
+                    getchar();
+                    fgets(p1[i].date.jour, sizeof(p1[i].date.jour), stdin);
+                    printf("Mois : ");
+                    getchar();
+                    fgets(p1[i].date.mois, sizeof(p1[i].date.mois), stdin);
+                    printf("AnnÃ©e : ");
+                    getchar();
+                    fgets(p1[i].date.annee, sizeof(p1[i].date.annee), stdin);
+                }
+                break;
+        
+            case 2 :
+            printf("veuillez saiser le reference de patient :");
+            scanf("%s",mod_par_refer);
+                if(strcmp(p1[i].reference,mod_par_refer)==0){
+                    printf("le reference %s est dans la liste de reservation :",mod_par_refer);
+                    printf("\tles parametres de modification\n");
+                    printf("1/.modification de nom .\n");
+                    printf("2/.modification de prenom .\n");
+                    printf("3/.modification de numero de telephone .\n");
+                    printf("4/.modification d'age .\n");
+                    printf("5/.modification de status .\n");
+                    printf("6/.modification de reference .\n");
+                    printf("7/.modification de date de reservation .\n");
+                    printf("veuillez saiser ton choice .");
+                    scanf("%d",&choice2);
+                    getchar();
+                    switch(choice2){
+                        case 1 :
+                        printf("tu es choisir modofication de nom .\n");
+                        printf("veuillez saiser le nouveau nom :");
+                        fgets(p1[i].nom,sizeof(p1[i].nom),stdin);
+                        printf("le modification a ete avec succes .\n");
+                        break;
+                        case 2 :
+                        printf("tu es choisir modofication de prenom .\n");
+                        printf("veuillez saiser le nouveau prenom :");
+                        fgets(p1[i].prenom,sizeof(p1[i].prenom),stdin);
+                        printf("le modification a ete avec succes .\n");
+                        break;
+                        case 3 :
+                        printf("tu es choisir modofication de nom .\n");
+                        printf("veuillez saiser le nouveau numero de telephone :");
+                        fgets(p1[i].telephone,sizeof(p1[i].telephone),stdin);
+                        printf("le modification a ete avec succes .\n");
+                        break;
+                        case 4 :
+                        printf("tu es choisir modofication de nom .\n");
+                        printf("veuillez saiser le nouveau age :");
+                        fgets(p1[i].age,sizeof(p1[i].age),stdin);
+                        printf("le modification a ete avec succes .\n");
+                        break;
+                        case 5 :
+                        printf("tu es choisir modofication de nom .\n");
+                        printf("veuillez saiser le nouveau status :");
+                        scanf("%d", &p1[i].statut);
+                        printf("le modification a ete avec succes .\n");
+                        break;
+                        case 6 :
+                        getchar();
+                        printf("tu es choisir modofication de nom .\n");
+                        printf("veuillez saiser le nouveau reference :");
+                        fgets(p1[i].reference,sizeof(p1[i].reference),stdin);
+                        printf("le modification a ete avec succes .\n");
+                        break;
+                        case 7 :
+                        printf("tu es choisir modofication de nom .\n");
+                        printf("veuillez saiser le nouveau date de reservation :");
+                        printf("jour :");
+                        fgets(p1[i].date.jour,sizeof(p1[i].date.jour),stdin);
+                        printf("mois :");
+                        fgets(p1[i].date.mois,sizeof(p1[i].date.mois),stdin);
+                        printf("annee :");
+                        fgets(p1[i].date.annee,sizeof(p1[i].date.annee),stdin);
+                        printf("le modification a ete avec succes .\n");
+                        break;
+                    }
+                }
+            }
+    } while (choice1 != 0); 
     return 0;
 }
+
